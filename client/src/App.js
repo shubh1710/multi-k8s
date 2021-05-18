6507f46967cd796eef03b3f886366e3a59e12c0c
root <kbansal@rbbn.com>
2021-05-18 09:19:20 +0000

    Modified Client Source Code

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-	  <h1 className="App-title">FIB Calculator</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
